19f0a032649149c110f07a0df0893bb3298371a6
ivanraj89 <ivanraj89@gmail.com>
2023-02-06 15:49:48 +0800

    Added Formode.Impulse

---
+++ Assets/Scripts/PracticeScriptQ4.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PracticeScriptQ4 : MonoBehaviour
+{
+    //Create a script that allows a game object to jump when the player presses the space bar.
+
+    [SerializeField]
+    private float jumpForce = 50f;
+
+    [SerializeField]
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            
+        }
+    }
+}
