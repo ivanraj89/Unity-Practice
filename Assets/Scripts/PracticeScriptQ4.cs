173154237acaf069bd4a0184874eab9466d331e3
ivanraj89 <ivanraj89@gmail.com>
2023-02-08 15:27:08 +0800

    Update PracticeScriptQ4.cs

--- Assets/Scripts/PracticeScriptQ4.cs
+++ Assets/Scripts/PracticeScriptQ4.cs
@@ -12,6 +12,8 @@ public class PracticeScriptQ4 : MonoBehaviour
     [SerializeField]
     private Rigidbody rb;
 
+    [SerializeField]
+
     // Start is called before the first frame update
     void Start()
     {
