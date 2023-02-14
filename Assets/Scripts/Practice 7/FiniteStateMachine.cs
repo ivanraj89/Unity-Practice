489e6d6b16987bd4cdf0f4160143ea2d5e491954
ivanraj89 <ivanraj89@gmail.com>
2023-02-14 12:07:12 +0800

    FiniteStateMachine

--- Assets/Scripts/Practice 7/FiniteStateMachine.cs
+++ Assets/Scripts/Practice 7/FiniteStateMachine.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class FiniteStateMachine : MonoBehaviour
 {
     public Transform[] patrolPoints;
-    public float patrolSpeed;
-    public float chaseSpeed;
+    public float patrolSpeed = 2f;
+    public float chaseSpeed = 5f;
 
     private int currentPatrolPoint = 0;
     public Transform targetObject;
@@ -22,7 +22,14 @@ public class FiniteStateMachine : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(chasing)
+        {
+            Chase();
+        }
+        else
+        {
+            Patrol();
+        }
     }
 
     void Patrol()
@@ -32,10 +39,27 @@ public class FiniteStateMachine : MonoBehaviour
         {
             currentPatrolPoint = (currentPatrolPoint + 1) % patrolPoints.Length;
         }
+
+        Vector3 direction = targetObject.position - transform.position;
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, direction, out hit, 5f))
+        {
+            if(hit.transform == CompareTag("Player"))
+            {
+                chasing= true;
+            }
+        }
+        Debug.DrawLine(transform.position, transform.position + direction.normalized * 5f, Color.red);
+
     }
 
     void Chase()
     {
+        transform.position = Vector3.MoveTowards(transform.position,targetObject.position, chaseSpeed * Time.deltaTime);
 
+        if(Vector3.Distance(transform.position, targetObject.position) > 10f)
+        {
+            chasing = false;
+        }
     }
 }
