145929938849860a38d5733a0e5b88dbd623f425
ivanraj89 <ivanraj89@gmail.com>
2023-02-21 19:56:32 +0800

    Update

--- Assets/Scripts/Scripts_OOP/Practice Constructors/Constructor_Practice_5.cs
+++ Assets/Scripts/Scripts_OOP/Practice Constructors/Constructor_Practice_5.cs
@@ -1,7 +1,5 @@
-using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Constructor_Practice_5 : MonoBehaviour
