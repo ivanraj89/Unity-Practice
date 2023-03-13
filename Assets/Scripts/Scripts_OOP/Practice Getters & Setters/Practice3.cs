f2257e833c58c85ef815d64b4af05d65358c7571
ivanraj89 <ivanraj89@gmail.com>
2023-03-13 20:40:37 +0800

    Update practice 3

--- Assets/Scripts/Scripts_OOP/Practice Getters & Setters/Practice3.cs
+++ Assets/Scripts/Scripts_OOP/Practice Getters & Setters/Practice3.cs
@@ -3,16 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Practice3 : MonoBehaviour
-{
+{    
     public class Car : MonoBehaviour
     {
         private Vector3 m_Position;
 
         public Vector3 Position
         {
-            get { return transform.position}
+            get { return transform.position; }
             set { transform.position = value; }
         }
+
+        public Car(Vector3 m_Position)
+        {
+            this.Position = m_Position;
+        } 
+
+    }
+
+    private void Start()
+    {   
+        //Error message on trying Car car1 = new Car(transform.position);
+        //The reason you received the message 
+        //You are trying to create a MonoBehaviour using the 'new' keyword. 
+        //This is not allowed. MonoBehaviours can only be added using AddComponent()" 
+        //is because you tried to create a new instance of your Car class using the new keyword. 
+        //Since Car is a subclass of MonoBehaviour, it must be added to a game object as a component 
+        //using the AddComponent() method, and not created using new.
+
+        Car car1 = gameObject.AddComponent<Car>(); 
+        Vector3 newLocation = new Vector3(0, 1, 0);
+        car1.Position = newLocation;
     }
 
 }
